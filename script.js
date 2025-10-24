17db2b08a799d58f75c59a442e5be1ad63a8343f
fayez erum <fayezerum@gmail.com>
2025-10-24 11:12:37 +0530

    Update website

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function showBeach() {
   document.getElementById("recommendation").innerHTML = `
     <h2>Beach Destinations</h2>
-    <img src="https://source.unsplash.com/400x300/?beach" alt="Beach 1">
-    <img src="https://source.unsplash.com/400x300/?sea" alt="Beach 2">
+    <img src="https://timesofindia.indiatimes.com/travel/kerala/travel-guide/the-most-stunning-beaches-in-kerala/gs48538494.cms" alt="Beach 1">
+    <img src="https://www.google.com/imgres?q=beaches%20in%20india&imgurl=https%3A%2F%2Fwww.thestatesman.com%2Fwp-content%2Fuploads%2F2023%2F12%2Fwinter-beach-escapes.jpg&imgrefurl=https%3A%2F%2Fwww.thestatesman.com%2Ftravel%2Fwinter-beach-bliss-here-are-some-coastal-getaways-in-india-1503253308.html&docid=ymAsZOlutU27JM&tbnid=89jTZc93phlI0M&vet=12ahUKEwiOrO6mj7yQAxXMzTgGHc8lK_EQM3oECCoQAA..i&w=1200&h=800&hcb=2&ved=2ahUKEwiOrO6mj7yQAxXMzTgGHc8lK_EQM3oECCoQAA" alt="Beach 2">
   `;
 }
 
 function showTemple() {
   document.getElementById("recommendation").innerHTML = `
     <h2>Temple Destinations</h2>
-    <img src="https://source.unsplash.com/400x300/?temple" alt="Temple 1">
-    <img src="https://source.unsplash.com/400x300/?ancient-temple" alt="Temple 2">
+    <img src="https://www.google.com/imgres?q=temples%20in%20india&imgurl=https%3A%2F%2Fwww.trinityairtravel.com%2Fbblog%2F7980_second.jpg&imgrefurl=https%3A%2F%2Fwww.trinityairtravel.com%2Fblog%2F20-06-2021%2FIsland-Temples-in-India%2F67%2F&docid=X5Msh8kIkTYqKM&tbnid=EwwicH7vQyQNtM&vet=12ahUKEwi9qbrKj7yQAxXoRmcHHaQtJdwQM3oECB0QAA..i&w=1900&h=992&hcb=2&ved=2ahUKEwi9qbrKj7yQAxXoRmcHHaQtJdwQM3oECB0QAA" alt="Temple 1">
+    <img src="https://www.google.com/imgres?q=temples%20in%20india&imgurl=https%3A%2F%2Fimages.travelandleisureasia.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2024%2F09%2F19170952%2FKailasanathar-Temple-Kanchipuram-1600x900.jpg&imgrefurl=https%3A%2F%2Fwww.travelandleisureasia.com%2Fin%2Fdestinations%2Findia%2Ftemples-of-south-india%2F&docid=l9tAoMTuL0892M&tbnid=KZT8AAgXQk-ezM&vet=12ahUKEwi9qbrKj7yQAxXoRmcHHaQtJdwQM3oECCAQAA..i&w=1600&h=900&hcb=2&ved=2ahUKEwi9qbrKj7yQAxXoRmcHHaQtJdwQM3oECCAQAA" alt="Temple 2">
   `;
 }
 
 function showCountry() {
   document.getElementById("recommendation").innerHTML = `
     <h2>Explore by Country</h2>
-    <img src="https://source.unsplash.com/400x300/?india" alt="India">
-    <img src="https://source.unsplash.com/400x300/?japan" alt="Japan">
+    <img src="https://www.eiu.com/n/global-outlook-indias-political-and-economic-outlook-post-election/" alt="India">
+    <img src="https://www.google.com/imgres?q=japan&imgurl=https%3A%2F%2Fwww.state.gov%2Fwp-content%2Fuploads%2F2019%2F04%2FJapan-2107x1406.jpg&imgrefurl=https%3A%2F%2Fwww.state.gov%2Fcountries-areas%2Fjapan&docid=czbj6xeN2rdc5M&tbnid=qiBg_asNKVBJgM&vet=12ahUKEwiR5r6NkLyQAxX6UGwGHfJND28QM3oECBgQAA..i&w=2107&h=1406&hcb=2&ved=2ahUKEwiR5r6NkLyQAxX6UGwGHfJND28QM3oECBgQAA" alt="Japan">
   `;
 }
