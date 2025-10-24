d7d7eeca21db0768375871c090803b3e25083310
fayez erum <fayezerum@gmail.com>
2025-10-24 12:47:29 +0530

    Update website

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function showBeach() {
   document.getElementById("recommendation").innerHTML = `
     <h2>Beach Destinations</h2>
-    <img src="C:\Users\fayez\OneDrive\Documents\coursera web\beach2.jpg" alt="Beach 1">
-    <img src="winter-beach-escapes.jpg" alt="Beach 2">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Ramla_Bay.jpg/330px-Ramla_Bay.jpg" alt="Beach 1">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Beach_at_Fort_Lauderdale.jpg/250px-Beach_at_Fort_Lauderdale.jpg alt="Beach 2">
   `;
 }
 
 function showTemple() {
   document.getElementById("recommendation").innerHTML = `
     <h2>Temple Destinations</h2>
-    <img src="https://source.unsplash.com/400x300/?temple" alt="Temple 1">
-    <img src="https://source.unsplash.com/400x300/?ancient-temple" alt="Temple 2">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Maa_Mundeshwari_Devi.jpg/250px-Maa_Mundeshwari_Devi.jpg" alt="Temple 1">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Vishnupadh_Temple.jpg" alt="Temple 2">
   `;
 }
 
 function showCountry() {
   document.getElementById("recommendation").innerHTML = `
     <h2>Explore by Country</h2>
-    <img src="https://source.unsplash.com/400x300/?india" alt="India">
-    <img src="https://source.unsplash.com/400x300/?japan" alt="Japan">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Qutb_minar_ruins.jpg/250px-Qutb_minar_ruins.jpg" alt="India">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Series-N700a-Mt.Fuji.jpg/250px-Series-N700a-Mt.Fuji.jpg" alt="Japan">
   `;
 }
